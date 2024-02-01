c107add8f88668ea8f9431b2abb09b0450264ed2
Jeremi-Koloma <kolomajeremi60@gmail.com>
2024-02-01 13:05:17 +0000

    Data types in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("welcome !");
+/* ************************************** DATA TYPES **********
+Numer - String - Boolean - Undefined - Null - Symbol -BigInt
+ *********************************************************** */
+
+// Number
+let age = 10 ;
+console.log(age, typeof age);
+
+// String
+let firstName = "Jeremi";
+console.log(firstName, typeof firstName);
+
+// Boolean
+let isLoggin = true;
+console.log(isLoggin, typeof isLoggin);
+
+// Undefined
+let card;
+console.log(card, typeof card);

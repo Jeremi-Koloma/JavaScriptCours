ea8d2e0ad458b7c256fe2c08630ea56168e8f593
Jeremi-Koloma <kolomajeremi60@gmail.com>
2024-02-06 12:21:18 +0000

    Game logic almost ok

--- GuessMyNumber/guessNumberscript.js
+++ GuessMyNumber/guessNumberscript.js
@@ -1,13 +1,30 @@
 'use strict';
 
 // Math.random() / To generate Random numbers
-// Math.trunc() / To reduce the float numbers
+// Math.trunc() / To reduce the float numbers to int
 const secretNumber = Math.trunc((Math.random() * 20 + 1));
 console.log(secretNumber);
 
 // Get the check btn
-document.querySelector('.check').addEventListener('click', function (){
-    console.log('Btn check clicked !');
+document.querySelector('.check').addEventListener('click', function () {
+    let guessValue = Number(document.querySelector('.guess').value);
+
+    // Check if the user get then secret number
+    if (guessValue === secretNumber) {
+        document.querySelector('.message').textContent = "Congratulations !";
+    }
+    //  Check if the input value guessing is greater then secret number
+    else if (guessValue > secretNumber) {
+        document.querySelector('.message').textContent = 'To hight !';
+    }
+    // Check if the input value guessing is lower then secret number
+    else if (guessValue < secretNumber) {
+        document.querySelector('.message').textContent = 'To low !';
+    }
+
+
+
+
 }, false);
 
-document.querySelector('.number').textContent = 20;
+document.querySelector('.number').textContent = secretNumber;
